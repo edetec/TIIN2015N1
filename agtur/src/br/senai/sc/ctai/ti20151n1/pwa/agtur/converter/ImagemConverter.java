9b711dda36e9f4fc27ab25078d80857bfffe203e
Eder Soares <edetec@gmail.com>
2016-06-01 19:22:00 -0300

    [agtur] Correção para poder adicionar uma nova foto

--- agtur/src/br/senai/sc/ctai/ti20151n1/pwa/agtur/converter/ImagemConverter.java
+++ agtur/src/br/senai/sc/ctai/ti20151n1/pwa/agtur/converter/ImagemConverter.java
@@ -8,15 +8,19 @@ import javax.faces.convert.FacesConverter;
 import br.senai.sc.ctai.ti20151n1.pwa.agtur.model.entity.Imagem;
 import br.senai.sc.ctai.ti20151n1.pwa.agtur.model.rn.AlbumRN;
 
-@FacesConverter(forClass=Imagem.class)
+@FacesConverter(forClass = Imagem.class)
 public class ImagemConverter implements Converter {
 
 	@Override
 	public Object getAsObject(FacesContext arg0, UIComponent arg1, String valor) {
-		Long id = Long.parseLong(valor);
-		AlbumRN albumRN = new AlbumRN();
-		Imagem imagem = albumRN.buscarImagemPorId(id);
-		return imagem;
+		try {
+			Long id = Long.parseLong(valor);
+			AlbumRN albumRN = new AlbumRN();
+			Imagem imagem = albumRN.buscarImagemPorId(id);
+			return imagem;
+		} catch (NumberFormatException e) {
+			return new Imagem();
+		}
 	}
 
 	@Override
